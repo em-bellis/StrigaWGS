65200a60b058fe89b773ef7a601a28130354db17
Emily Bellis <emilysbellis@gmail.com>
2021-07-13 10:11:05 -0500

    edit file output

--- kmer-dist/list_to_fastq.py
+++ kmer-dist/list_to_fastq.py
@@ -1,8 +1,18 @@
 import sys
 
-### open file from first arg
-kmerFile = sys.argv[1]
-fastqFile = sys.argv[2]
+### open files from command line args
+inKmers = open(sys.argv[1], 'r')
+outKmers = open(sys.argv[2], 'w')
 
+### read through infile list and print reformatted fastq
+count = 0
+for line in inKmers:
+    outKmers.write(f'@{count}\n')
+    outKmers.write(f'{line.strip()}\n')
+    outKmers.write(f'+\n')
+    outKmers.write(f'{"Z"*31}\n')
+    count += 1
+    
 
-#inKmers = open(kmerFile, 'r')
+inKmers.close()
+outKmers.close()
