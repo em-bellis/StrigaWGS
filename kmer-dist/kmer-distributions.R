f83398e66d3b8f1e2c0a724859b8f27e56e12ef8
Emily Bellis <emilysbellis@gmail.com>
2021-07-13 09:43:15 -0500

    extract kmers w/GST >0.5

--- kmer-dist/kmer-distributions.R
+++ kmer-dist/kmer-distributions.R
@@ -2,3 +2,27 @@ library(tidyverse)
 pop = c("kisii","homabay")
 
 ### load and tidy input data
+for (i in 1:2) {
+  gst <- read.table(paste0('~/Desktop/Projects/StigaxSorghum/KenyaWGS/alan/',pop[i],'/GST_Output.txt'))
+  colnames(gst) <- c('kmer','HS','HT','DST','GST')
+  gst$pop <- paste0(pop[i])
+  if (i == 1) {
+    gst_all <- gst
+  } else {
+    gst_all <- rbind.data.frame(gst_all, gst)
+  }
+}
+
+### graph Gst
+ggplot(gst_all, aes(x = GST, col = pop)) +
+  geom_density() +
+  theme_classic()
+
+nrow(subset(gst_all, GST >= 0.5 & pop == "kisii"))/nrow(subset(gst_all, pop == "kisii")) # 4% of kisii kmers
+nrow(subset(gst_all, GST >= 0.5 & pop == "homabay"))/nrow(subset(gst_all, pop == "homabay")) # 0.6% of homa bay kmers
+
+### extract kmers with GST >0.5 for assembly
+kisii_kmer_list <- subset(gst_all, pop == "kisii" & GST >= 0.5)$kmer
+homabay_kmer_list <- subset(gst_all, pop == "homabay" & GST >= 0.5)$kmer
+write.table(kisii_kmer_list, "kisii_kmers_GST5.txt", row.names = F, col.names = F, sep = "\t", quote = F)
+write.table(homabay_kmer_list, "homabay_kmers_GST5.txt", row.names = F, col.names = F, sep = "\t", quote = F)
