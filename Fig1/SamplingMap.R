8cdb6893e0f5f9884c142c62cd1f038fa7c126fd
Emily Bellis <emilysbellis@gmail.com>
2021-07-15 16:21:30 -0500

    add lake

--- Fig1/SamplingMap.R
+++ Fig1/SamplingMap.R
@@ -1,19 +1,21 @@
 ## create a map showing locations of collections
 # https://ecodiv.earth/post/creating-a-map-with-inset-using-tmap/
-library(raster)
+rm(list=ls())
+#library(rgdal)
 library(tidyverse)
 # library(maps)
 # library(RColorBrewer)
-library(spData)
 library(sf)
-library(dplyr)
-library(grid)
+#library(spData)
 library(tmap)
 library(tmaptools)
-library(rmapshaper)
+#library(dplyr)
+#library(grid)
+#library(rmapshaper)
 library(jcolors)
+#library(raster)
 
-#setwd('/Users/emilybellis/Documents/GitHub/StrigaWGS/')
+#setwd('/Users/emilybellis/Documents/GitHub/StrigaWGS/Fig1')
 
 ## load in data points from sampling
 modern <- read.csv("Striga_GPS_sequenced.csv", header=T)
@@ -22,15 +24,15 @@ modern$Site <- as.factor(str_replace(modern$Site, "2","") %>% str_trim())
 modern$Type <- "modern"
 
 # gps coordinates for ancient samples
-herb <- read.csv('Striga_GPS_sequenced_Berlin.csv', header=T, nrow=5)
-herb <- herb %>% select(Lat, Lon)
-herb$Site <- c("Kisii","Homa Bay","Muhoroni","Maranda","Fort Ternan")
-herb$Host <- c("maize","maize","sugarcane","sorghum","NA")
-herb$Type <- "NHC"
+#herb <- read.csv('Striga_GPS_sequenced_Berlin.csv', header=T, nrow=5)
+#herb <- herb %>% select(Lat, Lon)
+#herb$Site <- c("Kisii","Homa Bay","Muhoroni","Maranda","Fort Ternan")
+#herb$Host <- c("maize","maize","sugarcane","sorghum","NA")
+#herb$Type <- "NHC"
 
 # subset to 68 sequenced
 modern_df <- SpatialPointsDataFrame(cbind.data.frame(modern$Lon, modern$Lat),modern,proj4string = CRS("+proj=longlat"))
-herb_df <- SpatialPointsDataFrame(cbind.data.frame(herb$Lon,herb$Lat),herb,proj4string = CRS("+proj=longlat"))
+#herb_df <- SpatialPointsDataFrame(cbind.data.frame(herb$Lon,herb$Lat),herb,proj4string = CRS("+proj=longlat"))
 mod_bb <- st_bbox(modern_df)
 mod_bb <- mod_bb + c(-0.15,-0.15,0.15,0.15)
 sg <- bb_poly(mod_bb)
@@ -45,15 +47,18 @@ kenya_smooth <- simplify_shape(kenya_sf, 0.01)
 sub_world <- subset(world, iso_a2=="KE"|iso_a2=="TZ"|iso_a2=="ET"|iso_a2=="UG"|iso_a2=="RW"|iso_a2=="BI")
 sub_world_sp <- as(sub_world, 'Spatial')
 
+## lake victoria from https://figshare.com/articles/dataset/Simulation_of_Lake_Victoria_Circulation_Patterns_Using_the_Regional_Ocean_Modeling_System_ROMS_/4017198
+vic <- st_read('victoria/contours.shp')
+vic_poly <- st_polygonize(vic$geometry[1])
+
 ## want this to just be points for the sampled locations; distinguish only herbarium & modern, color points same as PCoA
-mainmap <- tm_shape(kenya_smooth, bbox=mod_bb) +
+mainmap <- tm_shape(kenya_smooth, bbox=mod_bb + c(-0.15,-0.15,0.15,0.15)) +
   tm_polygons() +
 tm_shape(modern_df) + 
-  tm_symbols(size=0.7, col="Site", alpha=0.7, jitter=0.1, palette=jcolors(palette="pal5"), shape="Host") +
-  tm_legend(show=FALSE) + 
-tm_shape(herb_df) +
-  tm_symbols(size=0.8, col="black", shape="Host", shapes=c(0, 3, 1, 5)) +
-tm_scale_bar(position = c("left","bottom"))
+  tm_symbols(size=0.7, col="Site", alpha=0.7, jitter=0.1, palette=jcolors(palette="pal5"), shape="Host", legend.col.show = F) +
+  tm_text("Site", remove.overlap = T, xmod = 1.5, ymod = 1, size = 0.75) + 
+tm_scale_bar(position = c("left","bottom")) +
+  tm_layout(legend.outside = T)
 
 xy <- st_bbox(world %>%
                 filter(continent == "Africa"))
