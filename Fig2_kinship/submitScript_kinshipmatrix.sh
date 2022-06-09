5aaafdc28314dcd7ac2047be9c5113bcccc85678
Emily Bellis <emilysbellis@gmail.com>
2022-06-09 09:20:32 -0500

    Update submitScript_kinshipmatrix.sh

--- Fig2_kinship/submitScript_kinshipmatrix.sh
+++ Fig2_kinship/submitScript_kinshipmatrix.sh
@@ -9,8 +9,9 @@ cd $SLURM_SUBMIT_DIR
 
 module load gsl
 
-### estimate genotype posterior probabilities, I had to break these up to run separately and then combine before running ngsDist
-#angsd -GL 1 -out $SAMP -nThreads 32 -doCounts 1 -setMinDepth 50 -setMaxDepth 500 -doMajorMinor 1 -doMaf 1 -doGeno 8 -doPost 1 -SNP_pval 1e-6 -skipTriallelic 1 -bam bam.filelist -uniqueOnly 1 -remove_bads 1 -only_proper_pairs 0 -ref ../../StrigaHer_500.fasta -rf $SAMP
+##### estimate genotype posterior probabilities
+## I had to break these up to run separately (split list of contigs using 'split -l 5000 regions_2_5kb' and then 'cat' the geno.gz files before running ngsDist
+angsd -GL 1 -out $SAMP -nThreads 32 -doCounts 1 -setMinDepth 50 -setMaxDepth 500 -doMajorMinor 1 -doMaf 1 -doGeno 8 -doPost 1 -SNP_pval 1e-6 -skipTriallelic 1 -bam bam.filelist -uniqueOnly 1 -remove_bads 1 -only_proper_pairs 0 -ref ../../StrigaHer_500.fasta -rf $SAMP
 
-### compute pairwise genetic distances
+##### compute pairwise genetic distances
 ngsDist -verbose 1 -geno ALL.geno.gz -probs -n_ind 68 -n_sites 11615822 -o ALL.dist -n_threads 32
