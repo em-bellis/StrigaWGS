f1e0192e4f3398f09b1b1788cb3ef7e545aa7a9e
Emily Bellis <emilysbellis@gmail.com>
2022-06-09 09:01:01 -0500

    create R project

--- Fig1_mapxPCoA/kinship_mat.R
+++ Fig1_mapxPCoA/kinship_mat.R
@@ -17,9 +17,9 @@ site_df = data.frame(as.factor("Site" = labs$Site))
 rownames(site_df) = rownames(kin)
 
 ann_colors <- list(Host = c('finger millet' = "#1B9E77", maize = "aquamarine", sorghum = "plum", sugarcane = "darkorchid"),
-                   Site = c(Chemelil= "#8DA0CB", 'Homa Bay' = "#E78AC3", Kibos ="#A6D854", Kisii = "#FFD92F", Muhoroni ="#E5C494", Mumias ="#B3B3B3"))
+                   Site = c(Chemelil= "#628395", 'Homa Bay' = "#C5D86D", Kibos ="#DB2763", Kisii = "#17377A", Muhoroni ="#FC471E", Mumias ="#55DDE0"))
 
-p <- pheatmap(kin, color = magma(100), annotation_col = host_df, annotation_row = site_df, cutree_rows = 4, show_rownames = F, border_color = NA, cutree_cols = 4, 
+p <- pheatmap(kin, color = magma(100), annotation_col = host_df, annotation_row = site_df, show_rownames = F, border_color = NA,  
          annotation_colors = ann_colors, legend = F, fontsize_col = 4)
            
 pdf("FigX_heatmap.pdf", height = 4, width = 6.5)      
